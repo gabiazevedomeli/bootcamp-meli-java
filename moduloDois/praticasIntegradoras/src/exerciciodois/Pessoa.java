b8534bbfd874af3863b53d23bb315a23b3ec8693
Gabriela Azevedo <gabriela.azevedo@mercadolivre.com>
2022-06-26 19:45:13 -0300

    feat: Sobe resolução da prática integradora Cálculo de IMC

--- moduloDois/praticasIntegradoras/src/exerciciodois/Pessoa.java
+++ moduloDois/praticasIntegradoras/src/exerciciodois/Pessoa.java
@@ -1,5 +1,8 @@
 package exerciciodois;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Pessoa {
     String nome;
     int idade;
@@ -30,5 +33,41 @@ public class Pessoa {
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
+    }
+
+    public int calculaIMC(double pesoPessoa, double alturaPessoa) {
+        double calculoIMC = peso / (Math.pow(alturaPessoa, 2));
+        // https://www.devmedia.com.br/arredondando-numeros-em-java/28248
+        BigDecimal formatIMC = new BigDecimal(calculoIMC).setScale(2, RoundingMode.HALF_EVEN);
+        if (calculoIMC < 20) {
+            System.out.println(this.nome + " está abaixo do peso, com IMC de: " + formatIMC.doubleValue());
+            return -1;
+        }
+        if (calculoIMC > 20 && calculoIMC < 25) {
+            System.out.println(this.nome + " possui peso saudável, com IMC de: " + formatIMC.doubleValue());
+            return 0;
+        }
+        if (calculoIMC > 25) {
+            System.out.println(this.nome + " está com sobrepeso, com IMC de: " + formatIMC.doubleValue());
+            return 1;
+        }
+        return (int) calculoIMC;
+    }
+
+    public boolean ehMaiorIdade(int idadePessoa) {
+        // retorna um valor booleano para a pergunta: É maior de idade?
+        if (idadePessoa > 18) {
+            System.out.println(this.nome + " é maior de idade!!!");
+        } else {
+            System.out.println(this.nome + " é menor de idade!!!");
+        }
+        return idadePessoa > 18;
+    }
+
+    @Override
+    public String toString() {
+        // https://www.devmedia.com.br/como-criar-sobreposicoes-usando-o-metodo-tostring-em-java/29042
+        return "A pessoa: " + this.nome + " de ID " + this.id + " tem " + this.idade + " anos, pesa "
+                + this.peso + "kg e mede " + this.altura + "cm de altura.";
     }
 }
--- moduloDois/praticasIntegradoras/src/exerciciotresequatro/Main.java
+++ moduloDois/praticasIntegradoras/src/exerciciotresequatro/Main.java
@@ -10,5 +10,9 @@ public class Main {
         Pessoa pessoaTestaConstrutor = new Pessoa("Ângela Maria", 67);
         // O objeto só será criado caso seja criado um construtor para aceitar apenas estes dois parâmetros, nome e idade.
         // Quando o construtor é criado o erro se extingue.
+
+        System.out.println(pessoaConstrutorDois.calculaIMC(52, 1.80));
+        System.out.println(pessoaConstrutorDois.toString());
+        System.out.println(pessoaConstrutorDois.ehMaiorIdade(18));
     }
 }
