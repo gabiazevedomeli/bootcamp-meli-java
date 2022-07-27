d4cd013b2bbd6f4d56d86e8bd0bdc4a01be302f7
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-27 19:17:04 -0300

    feat: Criação do handler customizado para Bad Request

---
+++ moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/handler/TestCaseExceptionHandler.java
@@ -0,0 +1,27 @@
+package dh.meli.qabugs.handler;
+
+import dh.meli.qabugs.exceptions.TestCaseNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class TestCaseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(TestCaseNotFoundException.class)
+    public ResponseEntity<TestCaseExceptionsDetails> handleTestCaseNotFoundException(TestCaseNotFoundException exception) {
+        return new ResponseEntity<>(
+                TestCaseExceptionsDetails.builder()
+                        .message(exception.getMessage())
+                        .errorType("NOT_FOUND")
+                        .statusCode(404)
+                        .timestamp(LocalDateTime.now())
+                        .build(), HttpStatus.NOT_FOUND
+        );
+    }
+}
