8a269d6e80838e61651a12f97ac40838f5e923a3
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-27 19:13:45 -0300

    feat: Criação de método customizado findAllTestCases

--- moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/repository/TestCaseRepository.java
+++ moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/repository/TestCaseRepository.java
@@ -1,10 +1,15 @@
 package dh.meli.qabugs.repository;
 
 import dh.meli.qabugs.model.TestCase;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TestCaseRepository extends CrudRepository<TestCase, Long> {
 
+    @Query("from TestCase as testCase")
+    List<TestCase> findAllTestCases();
 }
---
+++ moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/service/TestCaseService.java
@@ -0,0 +1,2 @@
+package dh.meli.qabugs.service;public class TestCaseService {
+}
