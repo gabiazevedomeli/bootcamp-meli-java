f43f46e4adce046a4de6b04a1a83384f74aae7ea
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-31 17:24:57 -0300

    feat: Criação do método customizado para buscar por data

--- moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/repository/TestCaseRepository.java
+++ moduloSete/qa-bugs/src/main/java/dh/meli/qabugs/repository/TestCaseRepository.java
@@ -3,8 +3,10 @@ package dh.meli.qabugs.repository;
 import dh.meli.qabugs.model.TestCase;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -12,4 +14,30 @@ public interface TestCaseRepository extends CrudRepository<TestCase, Long> {
 
     @Query("from TestCase as testCase")
     List<TestCase> findAllTestCases();
+
+    @Query("select tc from TestCase as tc where tc.last_update = :date")
+    List<TestCase> findTestCaseByDate(@Param("date") Date date);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
